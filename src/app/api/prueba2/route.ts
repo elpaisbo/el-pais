b78edaa8d1174d2b1686546d413cd4eb87a2ef85
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-06-01 00:53:40 -0400

    test prerender

--- src/app/api/prueba2/route.ts
+++ src/app/api/prueba2/route.ts
@@ -3,35 +3,38 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 import mustache from "mustache";
 
-export async function GET() {
-    // const data = {
-    //     nombre: "John",
-    //     acciones: 10,
-    //     precio: 10000,
-    // };
+async function GET() {
+    const data = {
+        nombre: "John",
+        acciones: 10,
+        precio: 10000,
+    };
 
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    // const html = fs.readFileSync("src/app/api/prueba2/template.html", "utf8");
-    // const filledHtml = mustache.render(html, data);
-    // await page.setContent(filledHtml, { waitUntil: "domcontentloaded" });
+    const html = fs.readFileSync("src/app/api/prueba2/template.html", "utf8");
+    const filledHtml = mustache.render(html, data);
+    await page.setContent(filledHtml, { waitUntil: "domcontentloaded" });
 
-    // await page.emulateMediaType("screen");
+    await page.emulateMediaType("screen");
 
-    // const pdf = await page.pdf({
-    //     format: "A4",
-    //     printBackground: true,
-    //     margin: {
-    //         top: "20px",
-    //         bottom: "40px",
-    //         left: "20px",
-    //         right: "20px",
-    //     },
-    //     path: "result.pdf",
-    // });
+    const pdf = await page.pdf({
+        format: "A4",
+        printBackground: true,
+        margin: {
+            top: "20px",
+            bottom: "40px",
+            left: "20px",
+            right: "20px",
+        },
+        path: "result.pdf",
+    });
 
-    // await browser.close();
-    // console.log("Se genero el pdf");
+    await browser.close();
+    console.log("Se genero el pdf");
     return NextResponse.json("Se genero el pdf");
 }
+export default GET, {
+    noPrerender: true,
+};
