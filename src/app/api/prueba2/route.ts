25c6336b01cdaa5dc42cd80a27bed54e5bca7d93
Vanessa Tejerina <vanetejerina314@gmail.com>
2023-06-01 02:30:46 -0400

    Fixed

--- src/app/api/prueba2/route.ts
+++ src/app/api/prueba2/route.ts
@@ -1,37 +1,16 @@
 import { NextResponse } from "next/server";
 const puppeteer = require("puppeteer");
-import fs from "fs";
+const fs = require("fs");
 import mustache from "mustache";
-const nodemailer = require("nodemailer");
 
-type User = {
-    nombre: string;
-    email: string;
-    acciones: number;
-};
-
-export async function POST(request: Request) {
-    const user: User = await request.json();
-
-    let transporter = nodemailer.createTransport({
-        host: "m40.siteground.biz",
-        port: 465,
-        secure: true, // upgrade later with STARTTLS
-        auth: {
-            user: "info@acciones-elpaistarija.com",
-            pass: "381@1f@u1{6c",
-        },
-    });
-
+export async function GET() {
     const data = {
         nombre: "John",
         acciones: 10,
-        precio: 100,
+        precio: 10000,
     };
 
-    const browser = await puppeteer.launch({
-        headless: "new",
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     const html = fs.readFileSync("src/app/api/prueba2/template.html", "utf8");
@@ -54,34 +33,5 @@ export async function POST(request: Request) {
 
     await browser.close();
     console.log("Se genero el pdf");
-
-    let mailOptions = {
-        from: "info@acciones-elpaistarija.com",
-        to: user.email,
-        subject: "Sending Email using Node.js pdf",
-        attachments: [
-            {
-                filename: "result.pdf",
-                contentType: "application/pdf", // <- You also can specify type of the document
-                content: pdf, // <- Here comes the buffer of generated pdf file
-            },
-        ],
-    };
-    let newError = {};
-    let newResponse = {};
-
-    transporter.sendMail(mailOptions, function (error: any, info: any) {
-        if (error) {
-            newError = error;
-            console.log(error);
-        } else {
-            newResponse = info.response;
-            console.log("Email sent: " + info.response);
-        }
-    });
-    return NextResponse.json({
-        ...newError,
-        ...newResponse,
-        pdf: "Se genero el pdf",
-    });
+    return NextResponse.json("Se genero el pdf");
 }
