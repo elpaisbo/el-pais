bd70e5bc46cdfd3f652dec4109c3c7e2cff24b3c
elpaisbo <elpaisbo@gmail.com>
2025-07-10 16:55:15 -0400

    Update RegisterForm.tsx
    type, fechaNacimiento

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -13,6 +13,11 @@ import useCart from "@/app/hooks/useShoppingCart";
 type RegisterFormValues = {
     nombre: string;
     apellido: string;
+    fechaNacimiento: {
+        dia: string;
+        mes: string;
+        año: string;
+    };
     email: string;
     telefono: string;
     domicilio: string;
