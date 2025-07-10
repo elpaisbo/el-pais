dfcc264735f93d10625d4c8383b6d07f6e2afbc2
elpaisbo <elpaisbo@gmail.com>
2025-07-10 17:15:45 -0400

    Update RegisterForm.tsx
    tipo y mapeo para incluir fechaNacimiento

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -59,6 +59,8 @@ function RegisterForm() {
                     id={input.id}
                     label={input.label}
                     validations={input.validations}
+                    type={input.type}
+                    options={input.options}
                 />
             ))}
         </motion.form>,
