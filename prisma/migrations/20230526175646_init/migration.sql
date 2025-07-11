6cc5c08d6ec6d7aff30126e46a197460e7e1e527
elpaisbo <elpaisbo@gmail.com>
2025-07-11 18:37:24 -0400

    Update migration.sql
    feat: add fecha_nacimiento field to Registro table

--- prisma/migrations/20230526175646_init/migration.sql
+++ prisma/migrations/20230526175646_init/migration.sql
@@ -18,6 +18,7 @@ CREATE TABLE "Registro" (
     "adquisicion" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "telefono" TEXT NOT NULL,
     "ci" TEXT NOT NULL,
+    "fecha_nacimiento" DATE NOT NULL,
 
     CONSTRAINT "Registro_pkey" PRIMARY KEY ("id")
 );
