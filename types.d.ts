374932e0ffb5ca1021d607519bd92a266cd8c07f
elpaisbo <elpaisbo@gmail.com>
2025-07-14 13:48:19 -0400

    Update types.d.ts
    Added RegisterDB

--- types.d.ts
+++ types.d.ts
@@ -14,3 +14,16 @@ type Register = {
         año: string;
     };
 };
+
+type RegisterDB = {
+    nombre: string;
+    apellido: string;
+    nacionalidad: string;
+    domicilio: string;
+    id_compra: string;
+    acciones: number;
+    email: string;
+    telefono: string;
+    ci: string;
+    fecha_nacimiento: string;
+};
